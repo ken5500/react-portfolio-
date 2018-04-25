38c2ccb667f440e6c7f423850fc02dc43b1f11c1
Your Name <you@example.com>
2018-04-25 13:13:36 -0400

    react app added to app.js file

--- src/App.js
+++ src/App.js
@@ -4,9 +4,40 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>HelloWorld</h1>
-      </div>
+      <div className="demo-big-content">
+    <Layout>
+        <Header waterfall>
+            <HeaderRow title="Title">
+                <Textfield
+                    value=""
+                    onChange={() => {}}
+                    label="Search2"
+                    expandable
+                    expandableIcon="search"
+                />
+            </HeaderRow>
+            <HeaderRow>
+                <Navigation>
+                    <a href="#">Link</a>
+                    <a href="#">Link</a>
+                    <a href="#">Link</a>
+                    <a href="#">Link</a>
+                </Navigation>
+            </HeaderRow>
+        </Header>
+        <Drawer title="Title">
+            <Navigation>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+            </Navigation>
+        </Drawer>
+        <Content>
+            <div className="page-content" />
+        </Content>
+    </Layout>
+</div>
     );
   }
 }
