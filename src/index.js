3256acd022830bc22a1c18077430f5b52dd2033f
Your Name <you@example.com>
2018-04-25 13:37:01 -0400

    updated app.js import files

--- src/index.js
+++ src/index.js
@@ -5,5 +5,6 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
