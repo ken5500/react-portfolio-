84d3427aed83d6ee7499b1aeb7fdd533e270eafb
Your Name <you@example.com>
2018-04-26 10:04:07 -0400

    updated components

---
+++ src/componets/landing_page.js
@@ -0,0 +1,12 @@
+import react (Component) from 'react';
+
+class Landing extends Component {
+  render() {
+  return(
+    <div><h1>Landing Page</h1></div>
+   )
+  }
+ }
+
+
+export default Landing;
---
+++ src/componets/resume.js
@@ -0,0 +1,12 @@
+import react (Component) from 'react';
+
+class Resume extends Component {
+  render() {
+  return(
+    <div><h1>Resume Page</h1></div>
+   )
+  }
+ }
+
+
+export default Resume;
